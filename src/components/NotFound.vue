0b96420cf161e3acd49dcf8fecc859f46071b2da
kit rio <mario64aq@gmail.com>
2019-08-17 15:12:45 +0900

    Add Error component

---
+++ src/components/NotFound.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="notfound">
+    <H1>404</H1>
+    <p>page not found</p>
+    페이지를 찾을 수 없습니다.
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Notfound',
+  mounted() {
+    this.errorfunc()
+  }, // 컴포넌트 이름
+  methods: {
+    errorfunc: () => {
+      document.title = '404 Error'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
