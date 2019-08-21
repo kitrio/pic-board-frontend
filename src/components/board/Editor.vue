53cfe57094e6b4a1ec190996c60eebb16f974c52
kit rio <mario64aq@gmail.com>
2019-08-21 20:12:55 +0900

    Add Editor component

---
+++ src/components/board/Editor.vue
@@ -0,0 +1,73 @@
+<template>
+  <v-app id="editor">
+    <v-row>
+      <v-col
+        col="12"
+        md="2"
+        sm="1"
+      />
+      <v-col
+        col="12"
+        md="8"
+        sm="10"
+      >
+        <v-card-title>
+          오늘의 사진 하나
+        </v-card-title>
+        <v-form>
+          <v-textarea
+            
+          />
+          <v-btn
+            @click="sumbitText"
+            color="white"
+          >
+            올리기
+          </v-btn>
+          
+        </v-form>
+        <v-form>
+          <v-btn
+            type="button"
+            @click="submitImg"
+            color="blue">
+            사진 올리기
+          </v-btn>
+        </v-form>
+      </v-col>
+      <v-col
+        col="12"
+        md="2"
+        sm="1"
+      />
+    </v-row>
+  </v-app>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            rules: {
+                required: value => !! value || '글을 입력해주세요!',
+            }
+        }
+    },
+    methods: {
+        submitText(){
+            let form = new FormData()
+            form.append('memberid', this.userid)
+            form.append('password',this.password)
+            this.axios({
+                method: 'post',
+                url : '/list/write',
+                data: form,
+            })
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
--- src/router.js
+++ src/router.js
@@ -1,13 +1,44 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Login from './components/Login'
+import BoardList from './components/board/BoardList'
+import Editor from './components/board/Editor'
+import Contents from './components/board/Contents'
+import Notfound from './components/NotFound'
+import SignUp from './components/member/SignUp'
+import Login from './components/member/Login'
+
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
-  base: process.env.BASE_URL,
+  base: '/board',
   routes: [
+    {
+      path: '/board',
+      name: 'BoardList',
+      component: BoardList
+    },
+    { 
+      path: '/board/:boardnum',
+      name: 'Contents',
+      component: Contents
+    },
+    {
+      path: '/content/editor',
+      name: 'Editor',
+      component: Editor
+    },
+    {
+      path: '*',
+      name: 'Notfound',
+      component: Notfound
+    },
+    {
+      path: '/user/signup',
+      name: 'SignUp',
+      component: SignUp
+    },
     {
       path: '/user/login',
       name: 'Login',
