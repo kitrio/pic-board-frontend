64738bd96eeb7fe34f3a22a3a2bff9d49e060218
kit rio <mario64aq@gmail.com>
2019-08-24 14:26:49 +0900

    Add masonry lib

--- src/main.js
+++ src/main.js
@@ -4,8 +4,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import VueMasonry from 'vue-masonry-css'
+
 Vue.config.productionTip = false
 Vue.use(vuetify)
+Vue.use(VueMasonry)
 new Vue({
   router,
   store,
