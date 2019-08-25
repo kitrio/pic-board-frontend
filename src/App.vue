df83e8d3f13839490db4bfb1889aa48d0c8efcd8
kit rio <mario64aq@gmail.com>
2019-08-25 20:41:01 +0900

    Fix main layout footer

--- src/App.vue
+++ src/App.vue
@@ -2,50 +2,75 @@
   <v-app>
     <v-app-bar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Board</span>
-        <span class="font-weight-light">Pic</span>
+        <router-link :to="{name: 'BoardList'}">
+          <span>Board</span>
+          <span class="font-weight-light">Pic</span>
+        </router-link>
       </v-toolbar-title>
-      <v-app-bar-nav-icon />
       <v-spacer />
       <router-link
         v-if="memberInfo === null"
         :to="{name: 'Login'}"
       >
         <v-btn 
-        text>
+          text
+        >
           로그인
         </v-btn>
       </router-link>
       <v-btn
         v-else
-        @click="logOut"
         text
+        @click="logOut"
       >
         로그아웃
       </v-btn>
+      <v-label
+        v-if="memberInfo = !null"
+      />
       <router-link :to="{name: 'SignUp'}">
         <v-btn 
-        color="blue"
-        text
+          color="blue"
+          text
         >
           회원가입
         </v-btn>
       </router-link>
     </v-app-bar>
 
-    <v-content>
-      <router-view />
-    </v-content>
-    <v-footer
-      absolute
-      class="font-weight-medium"
+    <v-col
+        col="12"
+        md="2"
+        sm="2"
+      />
+    <v-col
+      col="12"
+      md="8"
+      sm="2"
     >
-      <v-col
-        cols="12"
+      <v-content>
+        <router-view />
+      </v-content>
+    
+    </v-col>
+    <v-col
+        col="12"
+        md="2"
+        sm="2"
+      />
+    
+    <v-row>
+      <v-footer
+        fixed
+        class="font-weight-medium"
       >
-        {{ new Date().getFullYear() }}
-      </v-col>
-    </v-footer>
+        <v-col
+          cols="12"
+        >
+          {{ new Date().getFullYear() }}
+        </v-col>
+      </v-footer>
+    </v-row>
   </v-app>
 </template>
 
