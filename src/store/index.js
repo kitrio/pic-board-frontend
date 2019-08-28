26057dcedea73bb8695c58ab442f1617ad3a959d
kit rio <mario64aq@gmail.com>
2019-08-28 19:46:53 +0900

    fix axios error
    Uncaught (in promise) TypeError: Cannot read property
    'protocol' of undefined

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,9 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import axios from "axios";
 import member from './member'
 import contents from './contents'
 import createPersistedState from 'vuex-persistedstate'
 Vue.use(Vuex)
-Vue.use(axios)
 export default new Vuex.Store({
   modules: {
     member,
