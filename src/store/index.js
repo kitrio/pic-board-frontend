9de95edcad48e130a00404596913a96caf21bfcc
kit rio <mario64aq@gmail.com>
2019-08-28 19:34:53 +0900

    Add vuex-persistState library
    vuex store saved local storage

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import axios from "axios";
 import member from './member'
-import content from './content'
+import contents from './contents'
+import createPersistedState from 'vuex-persistedstate'
 Vue.use(Vuex)
 Vue.use(axios)
 export default new Vuex.Store({
   modules: {
     member,
-    content
-  }
+    contents
+  },
+  plugins: [createPersistedState()]
 })
