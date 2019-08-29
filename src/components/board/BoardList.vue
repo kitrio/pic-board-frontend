ff154fa4bdc3afd3335df648b9124b219a528753
kit rio <mario64aq@gmail.com>
2019-08-29 21:25:29 +0900

    Add thumbnail grid

--- src/components/board/BoardList.vue
+++ src/components/board/BoardList.vue
@@ -1,21 +1,43 @@
 <template>
   <v-app id="board-list">
     <v-card-title>이번주 추천</v-card-title>
-    <v-container fluid grid-list-md>
-      <v-layout row wrap>
-        <v-flex xs12 sm6 md4 lg3 x3
+    <v-container
+      fluid
+      grid-list-md
+    >
+      <v-layout
+        row
+        wrap
+      >
+        <v-flex
           v-for="(item, index) in posts"
-            :key="index"
+          :key="index"
+          xs12
+          sm6
+          md4
+          lg2
+          x2
         >
-            <v-card>
-            <router-link :to="{ name: 'Contents', params: { num: item.boardNum}}">
-              <v-img
-                :src="`${imgPath}${item.fileAltName}`"
-                max-width="400px"
-                aspect-ratio="1.4"
-              />
-            </router-link>
+          <v-hover
+            v-slot:default="{ hover }"
+            open-delay="180"
+          >
+            <v-card
+              :elevation="hover ? 12 : 2"
+            >
+              <router-link :to="{ name: 'Contents', params: { num: item.boardNum}}">
+                <v-img
+                  :src="`${imgPath}${item.fileAltName}`"
+                  max-width="400px"
+                  aspect-ratio="1.4"
+                  class="white--text align-end"
+                  gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.3)"              
+                >
+                  <v-card-text>{{ item.title }}</v-card-text>
+                </v-img>
+              </router-link>
             </v-card>
+          </v-hover>
         </v-flex>
       </v-layout>
     </v-container>
@@ -29,7 +51,7 @@ export default {
     return {
       index: 5,
       posts: [],
-      imgPath: process.env.VUE_APP_FILE_URL
+      imgPath: process.env.VUE_APP_FILE_URL+'thumb_'
     };
   },
   mounted() {
