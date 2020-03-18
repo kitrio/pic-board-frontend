a2593c22a54c6a4e30104b2c1d0171135c1cde2d
kit rio <mario64aq@gmail.com>
2020-03-18 20:30:05 +0900

    Fix Security Alerts from git

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,32 @@
-// https://eslint.org/docs/user-guide/configuring
-
 module.exports = {
   root: true,
-  parserOptions: {
-    parser: 'babel-eslint',
-    "ecmaVersion": 6,
-    "ecmaFeatures": {
-    "experimentalObjectRestSpread": true
-    } 
-  },
   env: {
-    browser: true,
+    node: true
   },
   extends: [
-    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/recommended'
-    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+    'plugin:vue/essential',
+    '@vue/standard'
   ],
-  // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
-  // add your custom rules here
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
   rules: {
-    // allow async-await
-    'generator-star-spacing': 'off',
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    "space-before-function-paren": ["error", "never"],
-  }
+    'no-console': process.env.NODE_ENV === 'production' ? 'off' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  },
+  globals: {
+    VUE_APP_BASE_URL: false,
+    axios: false
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+      ],
+      env: {
+        jest: true
+      }
+    }
+  ]
 }
