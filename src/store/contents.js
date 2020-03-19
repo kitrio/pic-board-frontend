fb02e3058d4d8476f904428f47f5de9947fa324a
kit rio <mario64aq@gmail.com>
2020-03-20 02:21:25 +0900

    Refomat codelint

--- src/store/contents.js
+++ src/store/contents.js
@@ -1,56 +1,56 @@
 const state = {
-    content: null,
-    imgPath: null
+  content: null,
+  imgPath: null
 }
 
-const getters = { 
-        getContent: function(state) {
-            return state.content
-        },
-        getImg: function(state) {
-            return state.imgPath
-        }
-    }
+const getters = {
+  getContent: function (state) {
+    return state.content
+  },
+  getImg: function (state) {
+    return state.imgPath
+  }
+}
 
 const mutations = {
-    setContent(state, payload) {
-        state.content = payload;
-    },
-    setImg(state, payload) {
-        state.imgPath = payload
-    }
+  setContent (state, payload) {
+    state.content = payload
+  },
+  setImg (state, payload) {
+    state.imgPath = payload
+  }
 }
 
 const actions = {
-    imgSubmit({ commit }, payload) {
-        axios({
-            method: 'post',
-            header: {
-                'Content-Type': 'multipart/form'
-            },
-            url: '/list/content/write/image',
-            processData: false,
-            contentType: false,
-            data: payload
-        })
-        .then(response =>{
-            if(response.status === 200){
-                commit('setImg', response.data)
-            }
-        })
-        .catch(error => {
-            console.log(error)
-        })
-    },
-    contentAction({ commit }, payload) {
-        commit('setContent', payload)
-    }
+  imgSubmit ({ commit }, payload) {
+    axios({
+      method: 'post',
+      header: {
+        'Content-Type': 'multipart/form'
+      },
+      url: '/list/content/write/image',
+      processData: false,
+      contentType: false,
+      data: payload
+    })
+      .then(response => {
+        if (response.status === 200) {
+          commit('setImg', response.data)
+        }
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  },
+  contentAction ({ commit }, payload) {
+    commit('setContent', payload)
+  }
 }
 
 export default {
-    namespaced: true,
-    state,
-    getters,
-    actions,
-    mutations
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations
 }
--- src/store/member.js
+++ src/store/member.js
@@ -1,48 +1,47 @@
 const state = {
-    myInfo: null
+  myInfo: null
+}
+
+const getters = {
+  getMyInfo: function (state) {
+    return state.myInfo
   }
-  
-  const getters = {
-    getMyInfo: function (state) {
-      return state.myInfo
-    }
+}
+
+const mutations = {
+  setMyInfo (state, payload) {
+    state.myInfo = payload
   }
-  
-  const mutations = {
-    setMyInfo (state, payload) {
-      state.myInfo = payload
-    }
+}
+
+const actions = {
+  logIn ({ commit }, payload) {
+    commit('setMyInfo', payload)
+  },
+  logOut ({ commit }) {
+    axios.get('/member/logout')
+      .then(() => {
+        commit('setMyInfo', null)
+      })
+      .catch(() => {
+        commit('setMyInfo', null)
+      })
+  },
+  nicknameAction ({ commit }, payload) {
+    axios.post('/member/nickname')
+      .then((response) => {
+        alert(response.data + '님 로그인 되었습니다.')
+        this.$Vue.set(this.$store.state.member.myInfo, 'nickname', response.data)
+      }).catch(() => {
+
+      })
   }
-  
-  const actions = {
-    logIn ({ commit }, payload) {
-      commit('setMyInfo', payload)
-    },
-    logOut ({ commit }) {
-      axios.post('/member/logout')
-        .then(() => {
-        })
-        .catch(() => {
-          commit('setMyInfo', null)
-        })
-      commit('setMyInfo', null)
-    },
-    nicknameAction ({ commit }, payload) {
-      axios.post('/member/nickname')
-        .then((response) => {
-          alert(response.data + '님 로그인 되었습니다.')
-          this.$Vue.set(this.$store.state.member.myInfo, 'nickname', response.data)
-        }).catch(() => {
-  
-        })
-    }
-  }
-  
-  export default {
-    namespaced: true,
-    state,
-    getters,
-    actions,
-    mutations
-  }
-  
+}
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations
+}
